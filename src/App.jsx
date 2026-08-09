5d9d30d3fbdce09055c0d4d998669e4e7fb3330f
mahir954 <mk0914509@gmail.com>
2026-08-09 07:54:31 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Admin from "../admin/Admin";
 import AdminLogin from "../admin/AdminLogin";
-import { Routes, Route } from "react-router-dom";
-import Notification from "./pages/Notifications";
+
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -13,58 +13,69 @@ import Explore from "./pages/Explore";
 import Chat from "./pages/Chat";
 import Notifications from "./pages/Notifications";
 import ResetPassword from "./pages/ResetPassword";
-import Settings from "./pages/Settings";import BlockedUsers from "./pages/BlockedUsers";
+import Settings from "./pages/Settings";
+import BlockedUsers from "./pages/BlockedUsers";
 import SwitchAccount from "./pages/SwitchAccount";
 import MusicUpload from "./pages/MusicUpload";
 import MusicLibrary from "./pages/MusicLibrary";
-
-
-
 import NotFound from "./pages/NotFound";
-
 import Reels from "./pages/Reels";
+
 import Note from "./components/Note";
 
 function App() {
   return (
     <Routes>
-      <Route path="/admin"
-      element={<Admin />} />
+
+      {/* Admin */}
+      <Route path="/admin" element={<Admin />} />
       <Route path="/admin-login" element={<AdminLogin />} />
+
+      {/* Authentication */}
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+      {/* Profile */}
       <Route path="/profile" element={<Profile />} />
       <Route path="/profile/:userId" element={<Profile />} />
+
+      {/* Stories */}
       <Route path="/create-story" element={<CreateStory />} />
       <Route path="/story/:storyId" element={<StoryViewer />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route
-  path="/reset-password/:token"
-  element={<ResetPassword />}
-/>
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/blocked-users"
-      element={<BlockedUsers />} />
 
-      <Route path="*" element={<NotFound />} /><Route
-  path="/switch-account"
-  element={<SwitchAccount />}
-/>
-      <Route path="/reels"
-      element={<Reels />} />
+      {/* Explore */}
+      <Route path="/explore" element={<Explore />} />
+
+      {/* Chat */}
+      <Route path="/chat" element={<Chat />} />
+
+      {/* Notifications */}
+      <Route path="/notifications" element={<Notifications />} />
+
+      {/* Settings */}
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/blocked-users" element={<BlockedUsers />} />
+      <Route path="/switch-account" element={<SwitchAccount />} />
+
+      {/* Reels */}
+      <Route path="/reels" element={<Reels />} />
       <Route path="/reels/:reelId" element={<Reels />} />
-      <Route
-  path="/music-upload"
-  element={<MusicUpload />}
-/>
-<Route
-  path="/music-library"
-  element={<MusicLibrary />}
-/>
-<Route path="/notes" element={<Note />} />
+
+      {/* Music */}
+      <Route path="/music-upload" element={<MusicUpload />} />
+      <Route path="/music-library" element={<MusicLibrary />} />
+
+      {/* Notes */}
+      <Route path="/notes" element={<Note />} />
+
+      {/* Home */}
+      <Route path="/home" element={<Home />} />
+
+      {/* 404 - Always keep this last */}
+      <Route path="*" element={<NotFound />} />
+
     </Routes>
   );
 }
