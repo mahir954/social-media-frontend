c15a2877a8ce0a7447525f4fc92feba76e37bb60
mahir954 <mk0914509@gmail.com>
2026-08-09 02:15:18 -0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Navigate } from "react-router-dom";
+
 import Footer from "../components/layout/Footer";
 import Navbar from "../components/layout/Navbar";
 import Sidebar from "../components/layout/Sidebar";
@@ -6,6 +8,12 @@ import CreatePost from "../components/feed/CreatePost";
 import HomeFeed from "../components/feed/HomeFeed";
 
 function Home() {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/register" replace />;
+  }
+
   return (
     <>
       <Navbar />
@@ -16,6 +24,7 @@ function Home() {
         <div style={{ marginLeft: "20px" }}>
           <h1>Home Feed</h1>
           <p>Welcome to Social Media App</p>
+
           <Stories />
           <CreatePost />
           <HomeFeed />
