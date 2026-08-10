e9728d64d9ce63f3513702e4ab42bc6ef157ca9d
mahir954 <mk0914509@gmail.com>
2026-08-09 23:17:31 -0600

    Update AdminLogin.jsx

--- admin/AdminLogin.jsx
+++ admin/AdminLogin.jsx
@@ -9,7 +9,7 @@ const handleLogin = async (e) => {
 e.preventDefault();
 
 try {
-const response = await fetch("http://localhost:5000/api/admin/login", {
+const response = await fetch("https://social-media-backend-9fag.onrender.com/api/admin/login", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
