4b919c7bb05b5bcf4ffcc49da76bd985495eae3c
mahir954 <mk0914509@gmail.com>
2026-08-12 05:17:49 -0600

    Update Admin.jsx

--- admin/Admin.jsx
+++ admin/Admin.jsx
@@ -370,6 +370,7 @@ const handleChangeAdminPassword = async () => {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
         },
         body: JSON.stringify({
           currentPassword,
