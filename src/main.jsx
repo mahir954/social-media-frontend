fc40aac6b586b236c6da46720538eb4f9007fd24
mahir954 <mk0914509@gmail.com>
2026-08-12 01:15:58 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import "./styles/global.css";
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import "./styles/global.css";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
-    
+  </StrictMode>
+);
+
+// Register Fello Social Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "Fello Service Worker registered:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error(
+          "Fello Service Worker registration failed:",
+          error
+        );
+      });
+  });
+}
