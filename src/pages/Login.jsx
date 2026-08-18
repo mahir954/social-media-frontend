a2fed743e0d2642c93b3b6b4fb8e1c7f2157ba8a
mahir954 <mk0914509@gmail.com>
2026-08-18 05:23:14 -0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "../styles/Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
